c79b4e64b28a0f965c9ff610f6e690ccf9fa5f22
XIAOMING <xiaomingsu@outlook.com>
2021-07-10 05:56:09 -0400

    updated javadoc and optimized class unit test.

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"main","l":"BinaryOper"},{"p":"main","l":"Comparators"},{"p":"main","l":"Employee"},{"p":"main","l":"EmployeeManagement"},{"p":"<Unnamed>","l":"EmployeeTest"},{"p":"main","l":"TimeStamp"},{"p":"<Unnamed>","l":"TimeStampTest"}];updateSearchResults();
